ef0e1c5b62a4355a28e24119589505a0d308982a
ebaadraheem <143934811+ebaadraheem@users.noreply.github.com>
2024-03-31 22:14:54 +0500

    Completed.jsx

--- src/components/Completed.jsx
+++ src/components/Completed.jsx
@@ -23,7 +23,7 @@ const Completed = ({  Id,completed,setcompleted, name }) => {
             <div className=' flex justify-center mb-2 pl-3'>
                 <div className=' inline-grid w-2/3 gap-3 '>
 
-                    <div className=' flex gap-3 items-center '>
+                    <div className=' flex gap-3 items-center break-all'>
                         <h1 className='w-2/5 text-md'>{name}</h1>
 
                         <span className='flex h-7 w-9 hover:bg-blue-600 bg-blue-500 items-center justify-center rounded-md cursor-pointer' onClick={() => { Delete(Id) }}><MdDelete /></span>
